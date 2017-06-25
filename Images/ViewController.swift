eefa1710ab20ace5ad15f5724c8accae6b32ae4b
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-25 12:21:36 +0300

    last commit

--- Images/ViewController.swift
+++ Images/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var myNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changeClicked(_ sender: Any) {
+        
+        if myNumber == 0 {
+            imageView.image = UIImage(named: "kirk.jpg")
+            myNumber = myNumber + 1
+        } else if myNumber == 1 {
+            imageView.image = UIImage(named: "james.jpg")
+            myNumber = myNumber - 1
+        }
+        
     }
-
+    
+    
 
 }
 
